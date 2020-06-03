8f0c4c6ea8b25bae5969cad455f249e408295864
persilee <i@lishaoy.net>
2020-06-03 19:18:32 +0800

    🚀 init

---
+++ ctrip/src/main/java/net/lishaoy/android_ctrip/HomeActivity.java
@@ -0,0 +1,14 @@
+package net.lishaoy.android_ctrip;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
